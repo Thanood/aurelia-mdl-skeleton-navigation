9d2a2ae0ad0b775e4d9ddff0faa8c028fe2e68b5
Daniel Bendel <daniel.bendel@gmail.com>
2015-12-14 20:39:30 +0100

    added rippleDuration slider (this time visible)

--- src/aurelia-mdl/mdl-slider.js
+++ src/aurelia-mdl/mdl-slider.js
@@ -4,12 +4,13 @@ import 'material-design-lite';
 @inject(Element)
 @inlineView(`
     <template style="display: inline-block;">
-    <input ref="input" class="mdl-slider mdl-js-slider" type="range" min.bind="min" max.bind="max" value.bind="value" tabindex="0" />
+    <input ref="input" class="mdl-slider mdl-js-slider" type="range" min.bind="min" max.bind="max" step.bind="step" value.bind="value" tabindex="0" />
     </template>
 `)
 export class MdlSlider {
     @bindable() min = 0;
     @bindable() max = 100;
+    @bindable() step = 1;
     @bindable({
         defaultBindingMode: bindingMode.twoWay,
         defaultValue: 0
