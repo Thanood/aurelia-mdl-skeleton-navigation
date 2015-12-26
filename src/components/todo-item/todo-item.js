9567288975de4172b2331ebc40c44b7f6567ca63
Daniel Bendel <daniel.bendel@gmail.com>
2015-12-26 11:02:27 +0100

    todo items can now be edited

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -1,6 +1,18 @@
 import { bindable } from 'aurelia-framework';
 
 export class TodoItem {
+    static id = 0;
+    
     @bindable() todo;
-    constructor() {}
+    constructor() {
+        this.isEditing = false;
+        this.id = TodoItem.id++;
+    }
+    finishEditing() {
+        this.isEditing = false;
+        this.todo.title = this.input.value;
+    }
+    setEditing() {
+        this.isEditing = true;
+    }
 }
